44b7e55f0026629c07e6226883913ed76ca78e7c
dedeVilius <vilius@godigi.lt>
2023-06-26 11:44:36 +0300

    SupplierPage IF statement added

--- src/Pages/SuppliersPage/SuppliersPage.js
+++ src/Pages/SuppliersPage/SuppliersPage.js
@@ -20,30 +20,37 @@ const SuppliersPage = () => {
 
   const supplierItemElement = suppliers.map(supplier => {
     return (
-    <div className='supplier-item-wrapper' key={supplier.id}>
-      <div className='image'>
-        <img src={supplier.logoUrl} alt='Supplier logo'></img>
+      <div className='supplier-item-wrapper' key={supplier.id}>
+        <div className='image'>
+          <img src={supplier.logoUrl} alt='Supplier logo'></img>
+        </div>
+        <div className='description'>
+          <h2>{supplier.title}</h2>
+          <p>{supplier.slogan}</p>
+        </div>
+        <div className='contacts'>
+          <h2>Contacts</h2>
+          <span><Link to={`tel:` + supplier.phone}>{supplier.phone}</Link></span>
+          <span><Link to={`mailto:` + supplier.email}>{supplier.email}</Link></span>
+          <span>{supplier.address}</span>
+          <Link to={'/suppliers/' + supplier.id}><Button type='button'>Check this supplier</Button></Link>
+        </div>
       </div>
-      <div className='description'>
-        <h2>{supplier.title}</h2>
-        <p>{supplier.slogan}</p>
-      </div>
-      <div className='contacts'>
-        <h2>Contacts</h2>
-        <span><Link to={`tel:` + supplier.phone}>{supplier.phone}</Link></span>
-        <span><Link to={`mailto:` + supplier.email}>{supplier.email}</Link></span>
-        <span>{supplier.address}</span>
-        <Link to={'/suppliers/' + supplier.id}><Button type='button'>Check this supplier</Button></Link>
-      </div>
-    </div>
-  )})
+    )
+  })
 
   return (
     <Container>
       <Main>
         <h1>Suppliers</h1>
         <div className='supplier-list-wrapper'>
-          {supplierItemElement}
+          {suppliers && suppliers.length > 0 ? (
+            supplierItemElement
+          ) : (
+            <div className='no-suppliers-message'>
+              <h2>There is no sincerer love than the love of food...</h2>
+            </div>
+          )}
         </div>
       </Main>
     </Container>
